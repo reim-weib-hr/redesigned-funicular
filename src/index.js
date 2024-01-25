b45cabf85242eee6bfb858a3ca566481db3a24ae
reim-weib-hr <hrrmanager985@gmail.com>
2024-01-25 16:05:56 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Loader from './Loader/Loader';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const MarkdownPreview = lazy(() => import('./App'));
+
+const root = ReactDOM.createRoot(document.querySelector('#hey_app'));
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback={<Loader />}>
+      <MarkdownPreview />
+    </React.Suspense>
   </React.StrictMode>
 );
 
